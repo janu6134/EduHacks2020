670d10813f3db662fb3cf27854a543a68398249a
janu6134 <janaki.sathish12@gmail.com>
2020-10-18 11:32:49 -0700

    Update

--- create.py
+++ create.py
@@ -5,52 +5,53 @@ from PIL import Image
 
 def train():
     recognizer = cv2.face_LBPHFaceRecognizer.create()
-    harcascadePath = "haarcascade_frontalface_default.xml"
-    path = "TrainImages"
-    imagePaths=[os.path.join(path,f) for f in os.listdir(path)] 
-    faces=[]
-    Ids=[]
+    img_pathlist = [os.path.join("TrainImages", f) for f in os.listdir("TrainImages")] 
+    face_Student_Ids = []
+    face_images = []
     
-    for imagePath in imagePaths:
-        face_image=Image.open(imagePath).convert('L')
-        image_array=np.array(face_image,'uint8')
+    for imgpath in img_pathlist:
+        face_image = Image.open(imgpath).convert('L')
+        Student_Id = int(os.path.split(imgpath)[-1].split(".")[1])
+        face_images.append(np.array(face_image,'uint8'))
+        face_Student_Ids.append(Student_Id)
         
-        Id=int(os.path.split(imagePath)[-1].split(".")[1])
-        
-        faces.append(image_array)
-        Ids.append(Id)
-    recognizer.train(faces, np.array(Id))
+    recognizer.train(face_images, np.array(Student_Id))
+    
     recognizer.save("TrainedModel\Model.yml")
 
 def capture(studenttext, nametext):        
-    Id=(studenttext.get())
-    name=(nametext.get())
+    Student_Id = (studenttext.get())
+    Student_Name= (nametext.get())
     
-    videocapture = cv2.VideoCapture(0)
-    harcascadePath = "haarcascade_frontalface_default.xml"
-    detector=cv2.CascadeClassifier(harcascadePath)
-    count=0
+    vStudent_Ideocapture = cv2.VStudent_IdeoCapture(0)
+    detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    count = 0
+    
     while(True):
-        ret, img = videocapture.read()
+        ret, img = vStudent_Ideocapture.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = detector.detectMultiScale(gray, 1.3, 5)
-        for (x,y,w,h) in faces:
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)        
-            count=count+1
-            cv2.imwrite("TrainImages\ "+name +"."+Id +'.'+ str(count) + ".jpg", gray[y:y+h,x:x+w])
-            cv2.imshow('Register',img)
-        #wait for 100 miliseconds 
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        face_images = detector.detectMultiScale(gray, 1.3, 5)
+        
+        for (x,y,w,h) in face_images:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)        
+            count = count+1
+            cv2.imwrite("TrainImages\ "+Student_Name+"."+Student_Id +'.'+ str(count) + ".jpg", gray[y:y+h,x:x+w])
+            cv2.imshow('Register', img)
+
+        if cv2.waitKey(120) & 0xFF == ord('r'):
             break
         
-        elif count>100:
+        elif count > 100:
             break
-    videocapture.release()
-    cv2.destroyAllWindows() 
-    row = [Id , name]
-    with open('StudentDetails\StudentDetails.csv','a+') as csvFile:
+        
+    vStudent_Ideocapture.release()
+    cv2.destroyAllWindows()
+    
+    with open('Studentcsv\LoginDetails.csv','a+') as csvFile:
         writer = csv.writer(csvFile)
-        writer.writerow(row)
+        writer.writerow([Student_Id , Student_Name])
     csvFile.close()
+    
+    #Let us start training now that we have captured the images
     train()
     
